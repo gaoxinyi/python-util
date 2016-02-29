6673e4506f14526e58de8eb2338e37189be3bc69
gaoxinyi <gaoxy@vip.qq.com>
2016-02-29 14:57:53 +0800

    方法加入备注

--- orm/orm.py
+++ orm/orm.py
@@ -1,12 +1,16 @@
 #-*-coding:utf-8-*-
 
+# 引入mongodb的封装类
 import pymongo
 import sys
 
+# 设置字符编码为utf8
 reload(sys)
 sys.setdefaultencoding('utf8')
+# 连接mongodb数据库  1.2.3.4为ip。1234为端口。可以根据数据库的情况自定义。common_db是数据库名称。coll_sql为表名
 orm = pymongo.Connection("1.2.3.4",1234).common_db.coll_sql
 
+# 生成查询sql id->mdb数据库中的key。用来指定对应的查询语句 data->查询条件 order_by->默认是根据ID倒序。可以自定义排序方式。
 def search_sql(id,data=None,order_by=' order by ID desc'):
 	sqlPlus = orm.find_one({'key':id})
 	if data != None:
@@ -25,6 +29,7 @@ def search_sql(id,data=None,order_by=' order by ID desc'):
 		# print sqlPlus['sql']
 	return sqlPlus
 
+# 插入数据库 conn->连接对象 table->表名 data->插入数据
 def insert(conn,table,data=None):
 	if data == None or data.keys() == 0:
 		return False
@@ -44,6 +49,7 @@ def insert(conn,table,data=None):
 			conn.rollback()
 			return False
 
+# 插入数据(批量) conn->连接对象 table->表名 data->插入数据(列表)
 def inserts(conn,table,data=[]):
 	if len(data) == 0:
 		return False
@@ -64,6 +70,7 @@ def inserts(conn,table,data=[]):
 			conn.rollback()
 			return False
 
+# 修改数据 conn->连接对象 table->表名 data->修改信息 query->查询条件
 def update(conn,table,data=None,query=None):
 	if data == None or data.keys() == 0:
 		return False
@@ -90,21 +97,14 @@ def update(conn,table,data=None,query=None):
 		conn.commit()
 		return True
 
+# 修改或插入(如果数据不存在则插入反之则修改) conn->连接对象 table->表名 data->修改信息 query->查询条件
 def save(conn,table,data=None,query=None):
 	if searchOne(conn,'count.'+table,query)['COUNT'] == 0:
 		insert(conn,'t_'+table,data)
 	else:
 		update(conn,'t_'+table,data,query)
 
-def delSave(conn,table,data=[],page_no=1):
-	if len(data) != 0:
-		cursor = conn.cursor()
-		if int(page_no) == 1:
-			cursor.execute('truncate table ' + table)
-			cursor.close()
-		return inserts(conn,table,data)
-	else:	return False
-
+# 对结果集进行映射。column->映射的字段名 res->结果集
 def decodeSQL(column,res):
 	res_map = None
 	if res != None:
@@ -115,6 +115,8 @@ def decodeSQL(column,res):
 			idx += 1
 	return res_map
 
+# 查询满足条件的一条记录如果没有满足条件返回None
+# conn->连接对象 id->mdb数据库中的key。用来指定对应的查询语句 data->查询条件
 def searchOne(conn,id,data=None):
 	cursor = conn.cursor()
 	sqlPlus = search_sql(id,data)
@@ -123,6 +125,8 @@ def searchOne(conn,id,data=None):
 	cursor.close()
 	return res
 
+# 查询所有满足条件的记录如果没有满足条件返回None
+# conn->连接对象 id->mdb数据库中的key。用来指定对应的查询语句 data->查询条件 order_by->默认是根据ID倒序。可以自定义排序方式。
 def search(conn,id,data=None,order_by=' order by ID desc'):
 	cursor = conn.cursor()
 	sqlPlus = search_sql(id,data,order_by)
@@ -133,6 +137,8 @@ def search(conn,id,data=None,order_by=' order by ID desc'):
 	cursor.close()
 	return res
 
+# 查询所有满足当前分页条件的记录
+# conn->连接对象 id->mdb数据库中的key。用来指定对应的查询语句 data->查询条件 page->默认一页20条 order_by->默认是根据ID倒序。可以自定义排序方式。
 def searchPage(conn,id,data=None,page={'num':0,'size':20},order_by=' order by ID desc'):
 	cursor = conn.cursor()
 	sqlPlus = search_sql(id,data,order_by)
@@ -145,7 +151,7 @@ def searchPage(conn,id,data=None,page={'num':0,'size':20},order_by=' order by ID
 			res.append(decodeSQL(sqlPlus['column'],item))
 	cursor.close()
 	return {'count':count,'res':res}
-
+# 高级查询。具体参照Demo
 def searchSelf(conn,id,data=None):
 	cursor = conn.cursor()
 	sqlPlus = search_sql(id,data)
